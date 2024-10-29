3fb3cb433885a56c9a8747b8da6306399c189651
Deboramc05 <92049868+Deboramc05@users.noreply.github.com>
2024-10-29 14:24:29 -0300

    Criação Projeto Detona Ralph

---
+++ README.md
@@ -0,0 +1,30 @@
+
+## 👨‍💻 Desafio de Projeto Ri Happy : Reproduzindo o Jogo Detona Ralph
+Esse projeto foi proposto por Felipe Aguiar da plataforma educacional DIO.
+
+## 🖥 Preview:
+[Imagem do Projeto](./src/img/jogo.JPG)
+- Venha ver como ficou o projeto acessando [aqui aqui e jogue agora]()
+
+## 👨‍💻 Objetivo:
+- Reproduzir passo passo o projeto visto em aula.
+
+### 🛠️ Tecnologia Utilizada 🛠️
+Algumas das tecnologias e linguagens que utilizamosforam utilizadas:
+
+- Javascript
+- CSS
+- HTML
+- VSCode
+
+
+## 📌 Créditos
+Este jogo foi desenvolvido como parte de um projeto educacional da Digital Innovation One.
+
+
+
+
+
+
+
+
---
+++ src/main/main.js
@@ -0,0 +1,62 @@
+
+const state = {
+    view: {
+        squares: document.querySelectorAll(".square"),
+        enemy: document.querySelector(".enemy"),
+        timeleft: document.querySelector("#time-left"),
+        score: document.querySelector("#score"),
+    },
+    values: {
+        gameVelocity: 600,
+        hitPosition: 0,
+        result:0,
+        currentTime:30,
+    },
+    actions:{
+        timerId: setInterval(randomSquare, 600),
+        countDownTimerId: setInterval(countDown, 1000),
+    },
+}
+
+function countDown(){
+    state.values.currentTime--;
+    state.view.timeleft.textContent = state.values.currentTime;
+    if(state.values.currentTime <= 0 ){
+        clearInterval(state.actions.timerId);
+        clearInterval(state.actions.countDownTimerId);
+        alert("Seu Tempo Acabou!! Você fez um total de " + state.values.result)
+    }
+}
+function playSound(audioName){
+    let audio = new Audio(`./src/audio/${audioName}.m4a`)
+    audio.volume=0.2;
+    audio.play()
+}
+function randomSquare(){
+    state.view.squares.forEach((square)=>{
+        square.classList.remove("enemy");
+    });
+    let randaoNumber = Math.floor(Math.random()*9);
+    let randaoSquare = state.view.squares[randaoNumber];
+    randaoSquare.classList.add("enemy");
+    state.values.hitPosition = randaoSquare.id;
+}
+function addListenerHitBox(){
+    state.view.squares.forEach((square) =>{
+        square.addEventListener("mousedown",()=>{
+            if(square.id===state.values.hitPosition){
+                
+                state.values.result++;
+                state.view.score.textContent = state.values.result 
+                state.values.hitPosition= null;
+            playSound("hit");
+            }
+            
+        });
+    });
+}
+//função para estartar algumas funções do jogo
+function initialise(){
+    addListenerHitBox();
+}
+initialise();
